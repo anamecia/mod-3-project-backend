53c70803f94615cbc1a8bae3579e889ca464616c
ana <anameciarobalo@gmail.com>
2020-01-22 13:47:07 +0000

    locations migration, seed data, controller and resoources

--- mod-3-project-backend/db/seeds.rb
+++ mod-3-project-backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(
+    latitude: 51.520269,
+    longitude: -0.087066,
+    clue: "clue")
