2f994d7648cb05f5396122e44d89bd5f95645607
ana <anameciarobalo@gmail.com>
2020-01-22 17:47:40 +0000

    added more seed data

--- mod-3-project-backend/db/seeds.rb
+++ mod-3-project-backend/db/seeds.rb
@@ -6,7 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Location.create(
-    latitude: 51.520269,
-    longitude: -0.087066,
-    clue: "clue")
+Location.create(latitude: 51.520269,longitude: -0.087066,clue: "clue") #wasabi
+Location.create(latitude: 51.518526,longitude: -0.088289,clue: "clue") #underground
+Location.create(latitude: 51.517696,longitude: -0.088689,clue: "clue") #globe
+Location.create(latitude: 51.523931,longitude: -0.088491,clue: "clue") #park
+Location.create(latitude: 51.520850,longitude: -0.089869,clue: "clue") #santander bikes
+Location.create(latitude: 51.518689,longitude: -0.088212,clue: "clue") #boots
+Location.create(latitude: 51.518626,longitude: -0.085255,clue: "clue") #costa
+Location.create(latitude: 51.518329,longitude: -0.084754,clue: "clue") #pret a manger
+Location.create(latitude: 51.516833,longitude: -0.088749,clue: "clue") #santander
+Location.create(latitude: 51.519632,longitude: -0.087399,clue: "clue") #m&s
+Location.create(latitude: 51.517361,longitude: -0.088451,clue: "clue") #o2
+Location.create(latitude: 51.517113,longitude: -0.087007,clue: "clue") #pizza express
+Location.create(latitude: 51.517034,longitude: -0.088925,clue: "clue") #suited & booted
+
+
+
