6f84aebd3ae69c0d011bca7b659371fc3c686568
Lope Ariyo <lopeariyo@gmail.com>
2020-01-24 15:58:55 +0000

    ngrok commented out

--- mod-3-project-backend/config/environments/development.rb
+++ mod-3-project-backend/config/environments/development.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "fddd23ce.ngrok.io"
-
+  # config.hosts << "6017359c.ngrok.io"
   # Do not eager load code on boot.
   config.eager_load = false
 
